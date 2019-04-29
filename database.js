77fcdfd1b7160a84a3db6644d44d4d5be55f4088
Ariadna Villar Egea <ariadna.villar@etraveli.com>
2019-04-29 15:54:42 +0200

    Created products database

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ const Datastore = require("nedb");
 // collections. In this case it's usually a good idea to use autoload for all collections.
 const db = {};
 db.users = new Datastore("users.db");
-db.robots = new Datastore("robots.db");
+db.products = new Datastore("products.db");
 
 // You need to load each database (here we do it asynchronously)
 db.users.loadDatabase();
-db.robots.loadDatabase();
+db.products.loadDatabase();
 
 /*
 const user = {
@@ -17,7 +17,17 @@ const user = {
 
 db.users.insert(user);
 */
+/*
+const products = {
+  name: "Chair",
+  price: 100,
+  description: "Blue wonderful color chair"
+};
 
+db.products.insert(products);
+*/
+/*
 db.users.find({ name: "Conny" }, (err, docs) => {
   console.log(docs);
 });
+*/
